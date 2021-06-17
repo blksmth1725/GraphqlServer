42f05caaff62d120e8600214636530c09abb999f
Christian Sheen <csheen1725@gmail.com>
2021-06-17 09:50:28 -0400

    Importing all dependencies needed to create graphql schema

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const {
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLString,
+  GraphQLInt,
+} = require("graphql");
 const app = express();
 
+app.use("/graphql", graphqlHTTP({ graphiql: true }));
 app.listen(5000, () => console.log("Server Running"));
