3196c9285427c4e4e527ead444ed7436a74cc43e
Christian Sheen <csheen1725@gmail.com>
2021-06-17 12:32:37 -0400

    Random commit... currently working on Pedra App

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const {
   GraphQLSchema,
+  GraphQLObjectType,
   GraphQLList,
   GraphQLString,
   GraphQLInt,
 } = require("graphql");
 const app = express();
 
-app.use("/graphql", graphqlHTTP({ graphiql: true }));
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "HelloWorld",
+    fields: () => ({
+      message: { type: GraphQLString, resolve: () => "Hello World" },
+    }),
+  }),
+});
+
+app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
 app.listen(5000, () => console.log("Server Running"));
